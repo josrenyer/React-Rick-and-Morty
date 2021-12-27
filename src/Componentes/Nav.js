844790e4959358a9265a2e1d21e01ac2284be933
jose rangel <jl.rangel20@hotmail.com>
2021-12-26 20:02:44 -0500

    otra prueba

--- src/Componentes/Nav.js
+++ src/Componentes/Nav.js
@@ -17,7 +17,7 @@ export default function Nav(){
       	<ul> 
        		<Link to="/About"><li><img src="https://img.icons8.com/color/48/000000/about.png" alt=""/></li></Link>
        		<a href="https://github.com/josrenyer" target="_blank" rel="noopener noreferrer"><li><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt=""/></li></a>
-       		<a href="https://www.linkedin.com/in/jl-rangel-fullstackdeveloper/" target="_blank" rel="noopener noreferrer"><li><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt=""/></li></a>
+       		<a href="https://www.linkedin.com/in/jl-rangel-fullstackdeveloper/" rel="noreferrer" target="_blank"><li><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt=""/></li></a>
       	</ul>
       	</div>
       	<div className="icono">
