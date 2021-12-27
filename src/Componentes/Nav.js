e577c8c72bddfc093b921ae5b8bf7e9f35695c5d
jose rangel <jl.rangel20@hotmail.com>
2021-12-26 19:55:43 -0500

    otra prueba

--- src/Componentes/Nav.js
+++ src/Componentes/Nav.js
@@ -16,8 +16,8 @@ export default function Nav(){
       	<div className="menu" id={open ? "visible" : ""}>
       	<ul> 
        		<Link to="/About"><li><img src="https://img.icons8.com/color/48/000000/about.png" alt=""/></li></Link>
-       		<a href="https://github.com/josrenyer" target="_blank"><li><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt=""/></li></a>
-       		<a href="https://www.linkedin.com/in/jl-rangel-fullstackdeveloper/" target="_blank"><li><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt=""/></li></a>
+       		<a href="https://github.com/josrenyer" target="_blank" rel="noopener noreferrer"><li><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt=""/></li></a>
+       		<a href="https://www.linkedin.com/in/jl-rangel-fullstackdeveloper/" target="_blank" rel="noopener noreferrer"><li><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt=""/></li></a>
       	</ul>
       	</div>
       	<div className="icono">
